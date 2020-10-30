af10a4818b7c8a874ab6c11e9d136ee55c161b50
chaikub <73686686+chaiklz@users.noreply.github.com>
2020-10-30 15:47:46 +0700

    Update website.php

--- website.php
+++ website.php
@@ -7,8 +7,6 @@
         <p> Somchai Chongrachen Chai </p>
         <img src="https://raw.githubusercontent.com/chaiklz/www/main/pic1.jpg"> <img src="https://raw.githubusercontent.com/chaiklz/www/main/pic2.jpg">
         <br>
-        <a href="http://www.youtube.com"></a>
-        <h2>Joke</h2>
-        <p> yesterday i study itf with new teacher i think he look like ben chalatit either voice body </p>
+        <a href="http://www.youtube.com">Link</a>
     </body>
 </html>
