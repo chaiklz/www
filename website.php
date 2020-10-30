05323a06abe74785cb6f717f40f5857882c192c6
chaikub <73686686+chaiklz@users.noreply.github.com>
2020-10-30 15:48:30 +0700

    Update website.php

--- website.php
+++ website.php
@@ -5,7 +5,7 @@
     <body>
         <h1> Name </h1>
         <p> Somchai Chongrachen Chai </p>
-        <img src="https://raw.githubusercontent.com/chaiklz/www/main/pic1.jpg"> <img src="https://raw.githubusercontent.com/chaiklz/www/main/pic2.jpg">
+        <img src="https://raw.githubusercontent.com/chaiklz/www/main/pic1.jpg"/> <img src="https://raw.githubusercontent.com/chaiklz/www/main/pic2.jpg"/>
         <br />
         <a href="http://www.youtube.com">Link</a>
     </body>
