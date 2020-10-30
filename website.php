88d5ffdc897e71b08b8b50a8bde279b7b217af6d
chaikub <73686686+chaiklz@users.noreply.github.com>
2020-10-30 15:23:54 +0700

    Create website.php

---
+++ website.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title> Hello </title>
+    </head>
+    <body>
+        <h1> Name </h1>
+        <p> Somchai Chongrachen Chai </p>
+        <img src="pic1.jpg"> <img src="pic2.jpg">
+        <br>
+        <a href="http://www.youtube.com"></a>
+
+        <h2>Joke</h2>
+        <p> yesterday i study itf with new teacher i think he look like ben chalatit either voice body</p>
+    </body
+</html>
