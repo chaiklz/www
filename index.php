23147d3ab5b7fd35e2c256ebe1cf1fa0a2ade97e
chaikub <73686686+chaiklz@users.noreply.github.com>
2020-10-30 18:07:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <p> Somchai Chongrachen Chai </p>
         <img src="https://raw.githubusercontent.com/chaiklz/www/main/pic1.jpg"/> <img src="https://raw.githubusercontent.com/chaiklz/www/main/pic2.jpg"/>
         <br />
-        <a href="http://www.youtube.com">MyWebLikeLink</a>
+        <a href="http://www.youtube.com">My Favorite Link</a>
         <p> yesterday i study with my new teacher He name panwit He look like Ben Charatit either voice body </p>
     </body>
 </html>
